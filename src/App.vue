2da2f5245e0ed94a7d5d8da59aaa0398a93adb2e
ugodeughi <stefano.corradi@gmail.com>
2023-10-13 13:17:20 +0200

    [add] serachBar

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,17 @@ import { store } from './data/store';
 import Header from './components/Header.vue';
 import CardsContainer from './components/CardsContainer.vue';
 import Resuts from './components/partials/Resuts.vue';
+import SearchBar from './components/partials/SearchBar.vue';
+import Loader from './components/partials/Loader.vue';
 
 export default {
   name: 'App',
   components:{
     Header,
     CardsContainer,
-    Resuts
+    Resuts,
+    SearchBar,
+    Loader
   },
   data(){
     return{
@@ -20,13 +24,33 @@ export default {
   },
   methods:{
     getApi(){
-      axios.get(store.apiUrl)
+      store.isLoading = true;
+      axios.get(store.apiUrl,{
+        params:{
+          name: store.nameToSearch,
+          status: store.statusToSearch
+        }
+      })
         .then( res =>{
+          
           store.charctersList = res.data.results;
           console.log(store.charctersList);
+          store.isLoading = false;
+
+          // ciclo tutti i caratteri per prendere tutti gli stati
+          store.charctersList.forEach( char => {
+            // se lo status non è incluso nell'array degli status lo pusho
+            if(!store.statusList.includes(char.status)){
+              store.statusList.push(char.status)
+            }
+          })
+
         })
         .catch(err => {
-          console.log(err);
+          //console.log(err);
+          // in caso di errore (qnado l'API non trova nulla) resetto la lista dei caratteri
+          store.charctersList = [];
+          store.isLoading = false;
         })
 
     }
@@ -39,8 +63,10 @@ export default {
 
 <template>
   <Header titleStr="- Rick&Morty -" />
-  <CardsContainer />
-  <Resuts />
+  <SearchBar @startSearch="getApi" />
+  <Loader v-if="store.isLoading" title="Loarding Rick&Morty API" />
+  <CardsContainer v-else />
+  <Resuts v-if="!store.isLoading" />
   
 </template>
 
--- src/data/store.js
+++ src/data/store.js
@@ -3,5 +3,9 @@ import { reactive } from 'vue';
 // le proprietà dello'oggetto reactive sono proprità reattive 
 export const store = reactive({
   apiUrl: 'https://rickandmortyapi.com/api/character',
-  charctersList: []
+  charctersList: [],
+  nameToSearch: '',
+  statusToSearch: '',
+  isLoading: true,
+  statusList:[]
 })
